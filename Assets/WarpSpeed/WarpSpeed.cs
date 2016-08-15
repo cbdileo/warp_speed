025d0cd92e25349c21e3bc94ec2c37273efa3f18
Carleton DiLeo <cbdileo@gmail.com>
2016-08-14 19:45:24 -0600

    Initial commit

---
+++ Assets/WarpSpeed/WarpSpeed.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarpSpeed : MonoBehaviour {
+	public float WarpDistortion;
+	public float Speed;
+	ParticleSystem particles;
+	ParticleSystemRenderer rend;
+	bool isWarping;
+
+	void Awake()
+	{
+		particles = GetComponent<ParticleSystem>();
+		rend = particles.GetComponent<ParticleSystemRenderer>();
+	}
+
+	void Update()
+	{
+		if(isWarping && !atWarpSpeed())
+		{
+			rend.velocityScale += WarpDistortion * (Time.deltaTime * Speed);
+		}
+
+		if(!isWarping && !atNormalSpeed())
+		{
+			rend.velocityScale -= WarpDistortion * (Time.deltaTime * Speed);
+		}
+	}
+
+	public void Engage()
+	{
+		isWarping = true;
+	}
+
+	public void Disengage()
+	{
+		isWarping = false;
+	}
+
+	bool atWarpSpeed()
+	{
+		return rend.velocityScale < WarpDistortion;
+	}
+
+	bool atNormalSpeed()
+	{
+		return rend.velocityScale > 0;
+	}
+}
